3304bf175233e03d84d36e06084ebc965a25013a
Wopros46527351 <77889318+Wopros46527351@users.noreply.github.com>
2021-08-05 16:26:31 +0300

    up

--- mytest.py
+++ mytest.py
@@ -83,6 +83,42 @@ def bridge_points(LWP1,LWP2):
     print("fail")
     return (0,0)
 
+def top_points(LWP1):
+    """Находит индекс ближайшей точи в полилинии
+
+    Args:
+        LWP1 (lwp): линия1
+
+    Returns:
+        tuple: (индекс точки в первой линии)
+    """
+    links = []
+    for i1,v1 in enumerate(LWP1):
+        
+        dist = get_distance(v1[:2:],(0,0))
+        links.append((i1,v1,dist))
+    links = sorted(links,key = lambda x:x[4])
+    L = len(LWP1)
+    LWP1_lines = [(i1,i2) for i1,i2 in zip(range(0,L-1),range(1,L))]+[(L-1,0)] 
+    for link in links:
+        eligible = True
+        nono_index  = link[0]
+        for c,d in [i for i in LWP1_lines if nono_index not in i]:
+            A = link[2]
+            B = link[3]
+            C = LWP1[c]
+            D = LWP1[d]
+            #print(A,B,C,D)
+            if intersect(A,B,C,D):
+                eligible = False
+                break    
+        if eligible:
+            #print(link)
+            return link[:2:]
+    print("fail")
+    return (0,0)
+
+
 def make_connections(layer):
     """создаёт связи между всеми фигурами
 
@@ -226,9 +262,19 @@ print(lwps)
 
 
 
+<<<<<<< Updated upstream
 '''тут строятся мосты'''
 """
 msp.add_lwpolyline(bridge_len(1,3,figs))
+=======
+p1,p2 = bridge_points(figs[0].LWP,figs[1].LWP)
+s1=top_points(figs[0].LWP)
+print(s1,'top_points')
+dot1 = figs[0].LWP[p1]
+dot2 = figs[1].LWP[p2]
+s=[(dot1[0],dot1[1]),(dot2[0],dot2[1])]
+msp.add_lwpolyline(s)
+>>>>>>> Stashed changes
 f=figure(msp[-1])
 f.drawFigurePlt()
 """
